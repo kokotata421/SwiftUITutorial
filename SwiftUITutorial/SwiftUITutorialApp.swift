dcfb139dfdfb7225063a09fdafae8c8e072b22ff
Kota Kawanishi <kokotata421@gmail.com>
2022-08-14 12:42:06 +0900

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Kota Kawanishi on 2022/08/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
